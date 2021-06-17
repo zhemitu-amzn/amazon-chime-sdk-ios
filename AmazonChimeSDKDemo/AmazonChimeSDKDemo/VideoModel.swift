022669b768aa5054add06f5a0d01954a1c295208
Ziyi Zhang <ziyiz@amazon.com>
2021-06-17 16:15:23 -0700

    video tile limit 25

--- AmazonChimeSDKDemo/AmazonChimeSDKDemo/VideoModel.swift
+++ AmazonChimeSDKDemo/AmazonChimeSDKDemo/VideoModel.swift
@@ -10,7 +10,7 @@ import AmazonChimeSDK
 import UIKit
 
 class VideoModel: NSObject {
-    private let maxRemoteVideoTileCount = 16
+    private let maxRemoteVideoTileCount = 25
     private let remoteVideoTileCountPerPage = 6
 
     private var currentRemoteVideoPageIndex = 0
